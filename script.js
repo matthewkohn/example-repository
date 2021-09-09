a896f80d11f3de62fe37f298f95fa3f0e96a8439
Matthew <kohney319m@gmail.com>
2021-09-09 11:55:10 -0600

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,5 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+h2.style.color = "cyan";
+h2.style.textAlign = "center";
+document.querySelector("body").appendChild(h2);
